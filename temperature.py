c4096b8d16fa2425eaac5475bd9a92f8988dacbb
nikol <niko.lindforsgc@gmail.com>
2021-07-20 15:06:31 +0300

    timestampin luonti lisätty temperature.py

--- temperature.py
+++ temperature.py
@@ -1,20 +1,29 @@
 import requests
+import datetime
+
+#
+
 
 #kun haetaan mennyttä säätä, pitänee antaa paikkakunnan koordinaatit, ei nimeä, Kaisaniemi lat = 60.2, lon = 24.9
 
 #timestamp halutusta pvm:stä ja kelloajasta
 
+timestamp_generated = str(datetime.datetime(2021, 7, 15, 14, 00).timestamp()) #muutetaan str, jotta voidaan split() .0 poistamiseksi
+timestamp_splitted = timestamp_generated.split(".") 
+timestamp = timestamp_splitted[0] #valitaan haluttu eli listan 1.
+
 #muuttujat tähän
 
 lat = 60.2
 lon = 24.9
-#city_name = "Kaisaniemi"
 api_key = "e8b0924d5a0ce89fd664d80238a253c3"
-timestamp = 1626480000
+
+#testi timestamp devaukseen
+#timestamp = 1626480000
 
 
 
-def get_weather(api_key, city):
+def get_weather(api_key, lat,lon,timestamp):
     
     url = f"http://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={timestamp}&appid={api_key}&units=metric"
 
@@ -25,5 +34,5 @@ def get_weather(api_key, city):
     
     return temp
 
-print(get_weather(api_key,city_name))
+print(get_weather(api_key,lat,lon,timestamp))
 
