f3f869f2c0632c2cc43e19a166630eab335c6f0c
Andy333 <antti.lecklin@gmail.com>
2021-07-22 11:19:48 +0300

    error handling

--- main.py
+++ main.py
@@ -15,65 +15,58 @@ def user_interface():
         Token.Answer: '#2196f3 bold',
         Token.Question: '',
     })
+    query = 1
+    while query == 1:
+        try:
+       
+            print("Input your work hours")
 
-    print("Input your work hours")
+            #questions for user to input
+            questions = [
+                {
+                    'type': 'input',
+                    'name': 'project_name',
+                    'message': 'Project name:'
+                },
+                {
+                    'type': 'input',
+                    'name': 'task',
+                    'message': 'Task:'
+                },
+                {
+                    'type': 'input',
+                    'name': 'start_time',
+                    'message': 'Enter start time(yyyy-mm-dd hh:mm):',
+               
+                },
+                {
+                    'type': 'input',
+                    'name': 'end_time',
+                    'message': 'Enter end time(yyyy-mm-dd hh:mm):'
+                },
+                {
+                    'type':'list',
+                    'name':'continue',
+                    'message':'Do you want to add more hours?',
+                    'choices': ["yes","no"]
+                }
+            ]
 
-    #testing if user inputs are correct (not ready)
-    class start_time_validator(Validator):
-        def validate(self, document):
-            if len(document.text) == 12:
-                None
-            else:
-                raise ValidationError(
-                    print("please input in correct format"),
-                    #cursor_position=len(document.text) #move the cursor to empty space 
-                )
+            #User inputs to values
+            answers = prompt(questions, style=style)
+            project_name = answers.get('project_name')
+            task_explanation = answers.get('task')
+            start_time_str = answers.get('start_time')
+            end_time_str = answers.get('end_time')
+            continue_info = answers.get('continue')
 
-
-    #questions for user to input
-    questions = [
-        {
-            'type': 'input',
-            'name': 'project_name',
-            'message': 'Project name:'
-        },
-        {
-            'type': 'input',
-            'name': 'task',
-            'message': 'Task:'
-        },
-        {
-            'type': 'input',
-            'name': 'start_time',
-            'message': 'Enter start time(yyyy-mm-dd hh:mm):',
-            'validate': start_time_validator
-        },
-        {
-            'type': 'input',
-            'name': 'end_time',
-            'message': 'Enter end time(yyyy-mm-dd hh:mm):'
-        },
-        {
-            'type':'list',
-            'name':'continue',
-            'message':'Do you want to add more hours?',
-            'choices': ["yes","no"]
-        }
-    ]
-
-    #User inputs to values
-    answers = prompt(questions, style=style)
-    
-    project_name = answers.get('project_name')
-    task_explanation = answers.get('task')
-    start_time_str = answers.get('start_time')
-    end_time_str = answers.get('end_time')
-    continue_info = answers.get('continue')
-
-    start = datetime.strptime(start_time_str, "%Y-%m-%d %H:%M")
-    end = datetime.strptime(end_time_str, "%Y-%m-%d %H:%M")
-
-    return [project_name,task_explanation,start,end,continue_info]
+            start = datetime.strptime(start_time_str, "%Y-%m-%d %H:%M")
+            end = datetime.strptime(end_time_str, "%Y-%m-%d %H:%M")
+            query = 0
+            return [project_name,task_explanation,start,end,continue_info]
+        except:
+            print("Check the values and try again.")
+            
 
 if __name__ == "__main__":
     #If you want to test this part of the code then can test here 
